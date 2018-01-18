d3a1313fff62daf0df620b6a8f9e0a2e94383f56
laransom <laransom@gmail.com>
2018-01-18 07:47:20 -0500

    Remove unneeded Files

--- square_api/resources.py
+++
@@ -1,7 +0,0 @@
-from tastypie.resources import ModelResource
-from square_api.models import Result
-
-class ResultResource(ModelResource):
-    class Meta:
-        queryset = Result.objects.all()
-        resource_name = 'result'
